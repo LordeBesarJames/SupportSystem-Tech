dfee705f80a8eb6f04a95d4b26f95002d7b1d3b0
Rayhan Aurelia Pramana Rijal <rayhanits2023@gmail.com>
2024-10-07 17:51:11 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+package Tugas6;
+
+public class main {
+    public static void main(String[] args) {
+        supportsystem supportSystem = new supportsystem();
+        supportSystem.start();
+    }
+}
