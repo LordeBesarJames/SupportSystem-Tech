366d84d25ee1a4dbffceaa3e74950e703cb1cd2c
Rayhan Aurelia Pramana Rijal <rayhanits2023@gmail.com>
2024-10-07 17:48:39 +0700

    Create inputreader.java

---
+++ inputreader.java
@@ -0,0 +1,16 @@
+package Tugas6;
+import java.util.Scanner;
+
+public class inputreader {
+    private Scanner reader;
+
+    public inputreader() {
+        reader = new Scanner(System.in);
+    }
+
+    public String getInput() {
+        System.out.print("> ");  // print prompt
+        String inputLine = reader.nextLine();
+        return inputLine;
+    }
+}
